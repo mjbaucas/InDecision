5b6e5404e0e5412bc6399fb2e231b92a155fdd6d
MBaucas <mjbaucas@gmail.com>
2017-09-15 21:37:35 -0400

    Added new activity for create new list

---
+++ app/src/main/java/com/mjbaucas/indecision/CreateListActivity.java
@@ -0,0 +1,17 @@
+package com.mjbaucas.indecision;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mjbau on 2017-09-15.
+ */
+
+public class CreateListActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_list);
+    }
+}
--- app/src/main/java/com/mjbaucas/indecision/MainActivity.java
+++ app/src/main/java/com/mjbaucas/indecision/MainActivity.java
@@ -2,18 +2,29 @@ package com.mjbaucas.indecision;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 import butterknife.*;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    Button createList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.main);
+
+        createList = (Button) findViewById(R.id.new_list);
+        createList.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent createList = new Intent(MainActivity.this, CreateListActivity.class);
+                startActivity(createList);
+            }
+        });
+
         ButterKnife.bind(this);
         init();
     }
